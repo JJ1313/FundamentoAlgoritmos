c5654373a1818a944a2b2b7c0a2812ec9180fe45
jj <hgbeckerss@gmail.com>
2023-04-16 23:49:09 -0400

    Tarea Promedio y Area y perimetro

---
+++ areaPerimetroCuadeado.py
@@ -0,0 +1,6 @@
+lado = int(input("Ingresa lado: "))
+perimetro =4*lado 
+area = lado**2
+print(f"""
+Perimetro : {perimetro}
+Area : {area}""")
---
+++ promedioDistincion.py
@@ -0,0 +1,9 @@
+n1 = int(input("Ingresa nota 1: "))
+n2 = int(input("Ingresa nota 2: "))
+n3 = int(input("Ingresa nota 3: "))
+
+promedio = (n1 + n2 + n3)/3
+
+# print(f"Promedio : {promedio})")
+if promedio >= 60:
+    print(F"Aprobado a lo Hein...{promedio}")
