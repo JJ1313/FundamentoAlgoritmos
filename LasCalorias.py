94cd71fe86cec8f54232b0c376d5e7108989784d
JJ1312 <yeiyeithirteen@gmail.com>
2023-04-19 01:20:09 -0400

    Agrega solucion sin uso de ceil()

--- LasCalorias.py
+++ LasCalorias.py
@@ -8,7 +8,15 @@ while e != 0:
     totalConsumidas = 0
     for i in range(nComilonas):
       totalConsumidas += int(input(f'Ingrese calorias en comilona {i+1} : '))
-    nEntrenamientos = ceil(totalConsumidas / e)
+    # ------ Calcular la cantidad de entrenamientos ------
+    # Si el resultado no es exacto hay q encontrar el entero superior mas cercano
+
+    entrenamientosFloat = totalConsumidas / e # Resultado en float
+    nEntrenamientos = int(entrenamientosFloat) # Parte entera del resultado
+    if(entrenamientosFloat > nEntrenamientos): # Caso en que el resultado en float no es exacto
+      nEntrenamientos += 1  # Se le suma 1 al entero para compensar la fraccion faltante
+    # --- Misma funcion pero usando math.ceil()
+    #nEntrenamientos = ceil(totalConsumidas / e)
     print(f'''CASO {nCaso} :
 Fitnesstencio consume {e} calorias cada entrenamiento.
 Fitnesstencio consumio {totalConsumidas} calorias en sus {nComilonas} comilonas.
