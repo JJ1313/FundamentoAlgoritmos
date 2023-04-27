a57c69883ea7647aef750aeb99f0cfacadf07d03
jj <hgbeckerss@gmail.com>
2023-04-27 13:26:25 -0400

    Ejercicio de triangulo

---
+++ Triangulo.py
@@ -0,0 +1,31 @@
+
+
+while (True):
+  n =  int(input("Alto triangulo: "))
+  if(n>=1):
+    break
+
+
+for i in range(1, n+1):
+    for j in range(1, i+1):    
+      print(j, end=" ")
+    for k in range(j-1, 0, -1):
+      print(k, end=" ")
+    print()
+
+for i in range(1, n+1):
+  sumar = True
+  j=1
+  while(True):
+      if(j==0):
+         break
+      print(j, end=" ")
+      if j == i:
+        sumar= False
+      if(sumar):
+         j+=1
+      else:
+         j-=1
+  print()
+    
+      
